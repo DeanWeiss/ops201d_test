00a198a2a09afb1ee1160b432acec8b66f67d59d
Dean Weiss <dean.w.weiss@gmail.com>
2022-08-01 18:48:15 +0000

    Changed file names to a better system

---
+++ OpsChallenge06.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 06 Ops Challenge Solution
+# Author: Dean Weiss
+# Date of latest revision: 01 August 2022
+# Purpose: Create a script that detects if a file or directory exists, then creates it if it does not exist.
+#              - Your script must use at least one array, one loop, and one conditional.
+
+# Main
+
+
+
+# End
