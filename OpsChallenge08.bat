349aa7aa22100fbd020d9d5c705fb124c7f71b80
Dean Weiss <dean.w.weiss@gmail.com>
2022-08-04 00:29:12 +0000

    completed challenge08

---
+++ OpsChallenge08.bat
@@ -0,0 +1,10 @@
+# Script: Ops 201 Class 06 Ops Challenge Solution
+# Author: Dean Weiss
+# Date of latest revision: 02 August 2022
+# Purpose: Create a script that runs lshw and shows specific information
+
+# Main
+
+
+
+# End
