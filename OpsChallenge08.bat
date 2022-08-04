dc983fbb47b6060f0ff1a415cd47be6803bb8f59
Dean Weiss <dean.w.weiss@gmail.com>
2022-08-04 00:35:45 +0000

    commented out items

--- OpsChallenge08.bat
+++ OpsChallenge08.bat
@@ -1,10 +1,10 @@
-# Script: Ops 201 Class 06 Ops Challenge Solution
-# Author: Dean Weiss
-# Date of latest revision: 02 August 2022
-# Purpose: Create a script that runs lshw and shows specific information
+@REM # Script: Ops 201 Class 06 Ops Challenge Solution
+@REM # Author: Dean Weiss
+@REM # Date of latest revision: 02 August 2022
+@REM # Purpose: Create a script that runs lshw and shows specific information
 
-# Main
+@REM # Main
 
+@REM :: ROBOCOPY "C:\Users\j.thompson\Desktop\My Project Files" "C:\Users\j.thompson\Documents\Backup"
 
-
-# End
+@REM # End
