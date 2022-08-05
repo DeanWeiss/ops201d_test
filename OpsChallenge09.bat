51beb92ca8af93e07fc3b599145922cec61fe130
Dean Weiss <dean.w.weiss@gmail.com>
2022-08-05 21:40:59 +0000

    added challenge 10

--- OpsChallenge09.bat
+++ OpsChallenge09.bat
@@ -5,19 +5,19 @@
 
 @REM # Main
 
-# Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt.
+@REM # Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt.
 Get-EventLog -LogName System -After (Get-Date).AddHours(-24) >last_24.txt
 
-# Output all “error” type events from the System event log to a file on your desktop named errors.txt.
+@REM # Output all “error” type events from the System event log to a file on your desktop named errors.txt.
 Get-EventLog -LogName System -EntryType Error >errors.txt
 
-# Print to the screen all events with ID of 16 from the System event log.
+@REM # Print to the screen all events with ID of 16 from the System event log.
 Get-EventLog -LogName System | Where-Object {$_.EventID -eq 16}
 
-# Print to the screen the most recent 20 entries from the System event log.
+@REM # Print to the screen the most recent 20 entries from the System event log.
 Get-EventLog -Logname System -Newest 20
 
-# Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the … and show the entire text).
+@REM # Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the … and show the entire text).
 Get-EventLog -Logname System -Newest 500 | Group-Object -Property source | format-table -wrap -autosize
 
 @REM # End
---
+++ OpsChallenge10.bat
@@ -0,0 +1,38 @@
+@REM # Script: Ops 201 Class 10 Ops Challenge Solution
+@REM # Author: Dean Weiss
+@REM # Date of latest revision: 02 August 2022
+@REM # Purpose: Create a script that runs lshw and shows specific information
+
+@REM # Main
+
+@REM Print to the terminal screen all active processes ordered by highest CPU time consumption at the top.
+
+Get-Process | sort CPU -Descending
+
+@REM Print to the terminal screen all active processes ordered by highest Process Identification Number at the top.
+
+Get-Process | sort PID -Descending
+
+@REM Print to the terminal screen the top five active processes ordered by highest Working Set (WS(K)) at the top.
+
+Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 5
+
+@REM Start the process Internet Explorer (iexplore.exe) and have it open https://owasp.org/www-project-top-ten/.
+
+Start-Process iexplore https://owasp.org/www-project-top-ten/
+
+@REM Start the process Internet Explorer (iexplore.exe) ten times using a for loop. Have each instance open https://owasp.org/www-project-top-ten/.
+
+for ($i=0; $i -le 10; $i++) {Start-Process -file iexplore -arg 'https://owasp.org/www-project-top-ten/'}
+
+@REM Close all Internet Explorer windows.
+
+get-process iexplore | Stop-Process
+
+@REM Kill a process by its Process Identification Number. Choose a process whose termination won’t destabilize the system, such as Internet Explorer or MS Edge.
+
+taskkill /f /pid 7916
+
+@REM 7916 is what my PID was for iexplore. 
+
+@REM # End
